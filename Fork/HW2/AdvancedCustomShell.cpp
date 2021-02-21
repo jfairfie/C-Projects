b1c3b3be8bb087ecd82662ec73a1fe34ec8d92a7
Joshua <blockmanb@gmail.com>
2021-02-21 10:40:08 -0500

    Upated Fork/HW2

--- Fork/HW2/AdvancedCustomShell.cpp
+++ Fork/HW2/AdvancedCustomShell.cpp
@@ -26,7 +26,7 @@ void execArgs(char** command)
 
 }
 
-execPiped(char** command)
+void execPiped(char** command, char** command2)
 {
 	int pipefd[2];
 	pid_t child_pid1, child_pid2;
@@ -36,19 +36,42 @@ execPiped(char** command)
 		printf("Pipe failed");
 	}
 
-	p1 = fork();
+	child_pid1 = fork();
 	//Child 
 	if (child_pid1 == 0)
 	{
-
+		close (pipefd[0]);
+		dup2(pipefd[1], 1);
+		//execlp("ls", "ls", "-l", NULL);
+		execvp(command[0], command);
+	} else 
+	{
+		child_pid2 = fork();
+		if (child_pid2 == 0)
+		{
+			close(pipefd[1]);
+			dup2(pipefd[0], 0);
+			close(pipefd[0]);
+			//execlp("wc", "wc", "-l", NULL);
+			execvp(command2[0], command2);
+		}
 	}
+	waitpid(child_pid1, 0, 0);
+	close(pipefd[1]);
+	waitpid(child_pid2, 0, 0);
 }
 
 int main(int argc, char *argv[])
 {
+	int times = 5;
+
+	while(times > 0)
+	{
+
 	char* args[MAXLENGTH];
 	char command[MAXLENGTH];
-
+	fflush(stdout);
+	printf("myshl >> ");
 	cin.getline(command, MAXLENGTH);
 	string str;
 	int index = 0;
@@ -78,12 +101,23 @@ int main(int argc, char *argv[])
 		if (strcmp(args[index], "|") == 0)
 		{
 			//Process through pipe
+			args[index] = NULL;
+			char* piped[MAXLENGTH];
+
+			for (int i = index+1; i < MAXLENGTH; i++) {
+				piped[i - (index + 1)] = args[i];
+				args[i] = NULL;
+			}
+
+			execPiped(args, piped);
+			break;
 		} else if (index == 0)
 		{
 			//Process traditionally using fork() only
 			execArgs(args);
 		}
 	}
-
+	times--;
+	}
 	return 0;
 }
